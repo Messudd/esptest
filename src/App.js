b3a1be57f156e77e9475990dbb51279a90a90373
Messudd <90100309+Messudd@users.noreply.github.com>
2024-07-25 20:28:12 +0300

    btn_style

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     border: '1px solid transparent',
     padding: '5px 10px',
     borderRadius: '4px',
+    width: '100%',
     color: '#fff',
     fontSize: '1rem',
     fontWeight: 'bold'
@@ -25,7 +26,7 @@ function App() {
   };
 
   return (
-    <div style={{ display: 'flex', width:'15%', margin: '50px auto' ,flexDirection: 'column', gap: '15px'}}>
+    <div style={{ display: 'flex', width:'30%', margin: '50px auto' ,flexDirection: 'column', gap: '15px'}}>
       <button style={btnStyle} onClick={() => toggleLED('on')}>LED On</button>
       <button style={btnStyle} onClick={() => toggleLED('off')}>LED Off</button>
     </div>
