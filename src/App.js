010ecee5e8e019bfd198f08c06f355c392b9468f
Messudd <90100309+Messudd@users.noreply.github.com>
2024-07-26 01:42:55 +0300

    switch-update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
+import audio from './audio/light-switch.mp3';
+import './btn.css';
 import "./App.css";
 
 function App() {
-  const btnStyle = {
-    backgroundColor: "darkblue",
-    border: "1px solid transparent",
-    padding: "5px 10px",
-    borderRadius: "4px",
-    width: "100%",
-    color: "#fff",
-    fontSize: "1rem",
-    fontWeight: "bold",
-  };
+  const [value, setValue] = useState("");
+
+  const sound = new Audio(audio);
 
   const toggleLED = (state) => {
     axios
       .post("https://b4b0-82-222-123-251.ngrok-free.app/led", { state }) // Ngrok URL'sini buraya ekleyin
       .then((res) => {
-        console.log("status: ",res.data);
+        sound.play();
+        console.log("status: ", res.data);
+        setValue(res.data);
       })
       .catch((err) => {
         console.error("There was an error updating the LED state!", err);
@@ -38,20 +37,39 @@ function App() {
         }}
       />
       <div
+        className="body-div"
         style={{
           display: "flex",
-          width: "30%",
-          margin: "50px auto",
-          flexDirection: "column",
-          gap: "15px",
+          justifyContent: "center",
+          alignContent: "center",
+          margin: "50px 0",
+          gap: "50px",
         }}
       >
-        <button style={btnStyle} onClick={() => toggleLED("on")}>
-          Lamb on
-        </button>
-        <button style={btnStyle} onClick={() => toggleLED("off")}>
-          Lamb Off
-        </button>
+        <div
+          style={{
+            display: "flex",
+            width: "250px",
+            padding: "10px",
+            flexDirection: "column",
+            gap: "25px",
+          }}
+        >
+          <button className="hover-btn" onClick={() => toggleLED("on")}>
+            Lamb on
+          </button>
+          <button className="hover-btn" onClick={() => toggleLED("off")}>
+            Lamb Off
+          </button>
+        </div>
+        <div className="icon">
+          <FontAwesomeIcon
+            icon={faLightbulb}
+            bounce
+            fontSize="5rem"
+            style={{ color: value === 'on' &&  "#FFD43B"}}
+          />
+        </div>
       </div>
     </>
   );
